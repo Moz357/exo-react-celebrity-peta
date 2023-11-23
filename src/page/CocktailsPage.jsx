1ed155e07a028812d736d0d9b003342c9647f5a4
Moz357 <mohamed.bougrinat@lapiscine.pro>
2023-11-23 15:26:16 +0100

    useState commit

--- src/page/CocktailsPage.jsx
+++ src/page/CocktailsPage.jsx
@@ -1,32 +1,48 @@
 function CocktailsPage (){
-    // la fonction "fetch" nous permet de reccuperer une API qui nous donne un fichier json
+    
+    const [cocktails, setCocktails] = useState(null);
+    // => on utilise le hook "useState" pour déclarer une variable appelée “cocktails“ avec une valeur initiale de null, La fonction “setcocktails“ 
+        //est utilisée pour mettre à jour la valeur de "cocktails".
+
+    if (!cocktails) {
+        //  on mais une condition (ici nococktails)
+
+    // la fonction "fetch" nous permet de réccupérer une API qui nous donne un fichier json
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
-        .then((response) => {
-        // ici la variable reccupere les informations du fichier json
-    return response.json();
+        .then((cocktailsResponse) => {
+        //  dans l'attente de la variable pour réccupérer les informations du fichier json
+    return cocktailsResponse.json();
         })
-        .then ((json) => {
-            // ici on reccupere le fichiers json
-            console.log(json);
+        .then ((cocktailsInJs) => {
+            // dans l'attente de réccupérer le fichiers json pour le convertir
+            console.log(cocktails)
+            setCocktails(cocktails);
+            // on stock la variable cocktails
         });
         
 
-    return <p>test</p>
+        return <main>{cocktails ? <article>cocktails prêts</article> : <p>Cocktails en cours de chargement</p>}</main>;
 }
 export default CocktailsPage;
 
 /**
- * function CocktailsPage() {
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      console.log(json);
-    });
+*import { useState } from "react";
 
-  return <p>test</p>;
+function CocktailsPage() {
+  const [cocktails, setCocktails] = useState(null);
+
+  if (!cocktails) {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+      .then((coktailsResponse) => {
+        return coktailsResponse.json();
+      })
+      .then((cocktailsInJs) => {
+        console.log(cocktailsInJs);
+        setCocktails(cocktailsInJs);
+      });
+  }
+
+  return <main>{cocktails ? <article>cocktails prêts</article> : <p>Cocktails en cours de chargement</p>}</main>;
 }
 
-export default CocktailsPage;
- */
+  export default CocktailsPage;
